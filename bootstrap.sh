a36aba2b0d7e9743f8603fd7524ad4ba6cfe1e56
Mark E. DeYoung <mark.e.deyoung@gmail.com>
2020-09-07 22:12:54 +0300

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+yum -y install openssh-server net-tools
+systemctl enable sshd
+
+# Infiniband support
+yum -y groupinstall 
+"Infiniband Support"
+systemctl enable rdma
+
+yum -y update
