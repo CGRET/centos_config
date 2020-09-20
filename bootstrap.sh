f27e88e6ac58038a612e64376c0f9e7b536838ae
Mark E. DeYoung <mark.e.deyoung@gmail.com>
2020-09-20 16:26:54 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-yum -y install openssh-server net-tools nano
+yum -y install openssh-server net-tools nano wget gcc rpm-build kernel-devel-$(uname-r)
 systemctl enable sshd
 
 # Infiniband support
 yum -y groupinstall "Infiniband Support"
 systemctl enable rdma
+yum -y install mstflint
 
 # Install and configure avahi (AKA Bonjour)
 yum -y install avahi-daemon avahi-tools
